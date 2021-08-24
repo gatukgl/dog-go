a72666b1e9215f84e10a808b3bc4b9d5925f261e
Gatuk Chattanon <gatukgl@gmail.com>
2021-08-24 19:46:28 +0700

    Separate fizzbuzz package and define person type

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gatukgl/dog-go/fizzbuzz"
+)
+
+type person struct {
+	name  string
+	money float64
+}
+
+func main() {
+	fmt.Println("vim-go")
+	var x string = "Helloworld"
+	fmt.Println(x)
+	y := 10
+	fmt.Println(y)
+
+	var number int = 1
+	result := fizzbuzz.Fizzbuzz(number)
+	fmt.Println(result)
+
+	var gatuk person
+	gatuk.name = "gatuk"
+	gatuk.money = 2.2
+	fmt.Println(gatuk)
+
+	mac := person{
+		name:  "mac",
+		money: 3.0,
+	}
+	fmt.Println(mac)
+}
